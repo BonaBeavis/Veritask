68f00a0939bb16af967f731d95b7ed0af98066b9
Beavis <gordian.dziwis@gmail.com>
2016-07-11 17:30:32 +0200

    Cleans up Application

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,7 @@ package controllers
 import javax.inject.{Inject, Singleton}
 
 import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.libs.Jsonp
-import play.api.libs.json.Json
 import play.api.mvc._
-import play.api.routing.JavaScriptReverseRouter
 
 /**
   * Instead of declaring an object of Application as per the template project, we must declare a class given that
@@ -14,45 +11,18 @@ import play.api.routing.JavaScriptReverseRouter
   *
   */
 @Singleton
-class Application @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
+class Application @Inject()(val messagesApi: MessagesApi)
+  extends Controller with I18nSupport {
 
   def index = Action {
     Ok(views.html.index())
-  }
-
-  def template(callback: String) = Action {
-    val json = Json.obj("html" -> Json.toJson(views.html.widget.render().toString))
-    Ok(Jsonp(callback, json))
-  }
-
-  def getTemplate = Action {
-    Ok("r")
   }
 
   def widgetHTML = Action {
     Ok(views.html.widget())
   }
 
-//  def getTask = Action.async {
-//    TaskDao.findRandom() map {
-//      case Some(task) => Ok(Json.toJson(task))
-//      case None => Ok("nothing found")
-//    }
-////    val template = views.html.template(task)
-////    val json = Json.obj("html" -> Json.toJson(views.html.template(task).render().toString))
-////    Ok(json)
-//  }
-
   def widget = Action { request =>
     Ok(views.js.widget.render(request))
-  }
-
-  def javascriptRoutes = Action { implicit request =>
-    Ok(
-      JavaScriptReverseRouter("jsRoutes")(
-        routes.javascript.Application.template,
-        routes.javascript.Assets.versioned
-      )
-    ).as("text/javascript")
   }
 }
